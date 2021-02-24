bf87b97e994cd19a592dc46e72a7a72db85199e0
Graham <grahamstarr@gmail.com>
2021-02-24 17:38:49 +1100

    Android store implementation - broken

--- android/src/main/java/com/interapptive/plugins/health/Health.java
+++ android/src/main/java/com/interapptive/plugins/health/Health.java
@@ -4,17 +4,32 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import com.getcapacitor.JSObject;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.DataSet;
+
+import org.json.JSONException;
+
+import java.util.concurrent.TimeUnit;
 
 public class Health {
 
     private Context context;
     private final String tag = "---- IA HEALTH PLUGIN";
+
+    // Google account to access the API
+    GoogleSignInAccount account;
 
     public Health(Context context) {
         this.context = context;
@@ -53,6 +68,57 @@ public class Health {
     }
 
     public Boolean requestAuth() {
-        return true;
+        return false;
+    }
+
+    public Boolean query() {
+        return false;
+    }
+
+    public Boolean store(JSObject data, DataType dt) throws JSONException {
+
+        long st = data.getLong("startDate");
+        long et = data.getLong("endDate");
+
+        String sourceBundleId = context.getApplicationContext().getPackageName();
+        if (data.has("sourceBundleId")) {
+            sourceBundleId = data.getString("sourceBundleId");
+        }
+
+        DataSource dataSrc = new DataSource.Builder()
+                .setAppPackageName(sourceBundleId)
+                .setDataType(dt)
+                .setType(DataSource.TYPE_RAW)
+                .build();
+
+        DataSet.Builder dataSetBuilder = DataSet.builder(dataSrc);
+        DataPoint.Builder dataPointBuilder = DataPoint.builder(dataSrc);
+        dataPointBuilder.setTimeInterval(st, et, TimeUnit.MILLISECONDS);
+
+        if (dt.equals(DataType.TYPE_HEIGHT)) {
+            String value = data.getString("value");
+            float height = Float.parseFloat(value);
+            dataPointBuilder.setField(Field.FIELD_HEIGHT, height);
+        } else if (dt.equals(DataType.TYPE_WEIGHT)) {
+            String value = data.getString("value");
+            float weight = Float.parseFloat(value);
+            dataPointBuilder.setField(Field.FIELD_WEIGHT, weight);
+        } else if (dt.equals(DataType.TYPE_BODY_FAT_PERCENTAGE)) {
+            String value = data.getString("value");
+            float perc = Float.parseFloat(value);
+            dataPointBuilder.setField(Field.FIELD_PERCENTAGE, perc);
+        }
+        dataSetBuilder.add(dataPointBuilder.build());
+
+        // --- UP TO HERE ---
+        Fitness.getHistoryClient(context, this.account)
+                .insertData(dataSetBuilder.build())
+                .addOnSuccessListener(r -> {
+                    callbackContext.success();
+                })
+                .addOnFailureListener(err -> {
+                    err.getCause().printStackTrace();
+                    callbackContext.error(err.getMessage());
+                });
     }
 }
--- android/src/main/java/com/interapptive/plugins/health/HealthPlugin.java
+++ android/src/main/java/com/interapptive/plugins/health/HealthPlugin.java
@@ -2,17 +2,39 @@ package com.interapptive.plugins.health;
 
 import android.content.Context;
 
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.DataSource;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 @CapacitorPlugin(name = "Health")
 public class HealthPlugin extends Plugin {
 
     private Context context;
     private Health implementation;
+
+    // Declare data types compatible with plugin
+    public static Map<String, DataType> datatypes = new HashMap<String, DataType>();
+
+    static {
+        datatypes.put("height", DataType.TYPE_HEIGHT);
+        datatypes.put("weight", DataType.TYPE_WEIGHT);
+        datatypes.put("fat_percentage", DataType.TYPE_BODY_FAT_PERCENTAGE);
+    }
 
     /**
      * Load Method
@@ -52,5 +74,75 @@ public class HealthPlugin extends Plugin {
             ret.put("message", "Google Services not installed or obsolete");
         }
         call.resolve(ret);
+    }
+
+    // https://gist.github.com/dariosalvi78/66aa2635abd02f4aa4899628daf74cc7#file-mainactivity-java-L90
+    @PluginMethod
+    public void requestAuth(PluginCall call) {
+        JSObject ret = new JSObject();
+        ret.put("result", implementation.requestAuth());
+        ret.put("message", "Not implemented yet.");
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void query(PluginCall call) {
+        JSObject ret = new JSObject();
+        ret.put("result", implementation.query());
+        ret.put("message", "Not implemented yet.");
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void store(PluginCall call) {
+        JSObject ret = new JSObject();
+        JSObject data = call.getData();
+
+        if(!data.has("startDate")) {
+            ret.put("result", false);
+            ret.put("message", "Missing argument startDate");
+            call.resolve(ret);
+        }
+
+        if(!data.has("endDate")) {
+            ret.put("result", false);
+            ret.put("message", "Missing argument endDate");
+            call.resolve(ret);
+        }
+
+        if(!data.has("value")) {
+            ret.put("result", false);
+            ret.put("message", "Missing argument value");
+            call.resolve(ret);
+        }
+
+        if(!data.has("sourceName")) {
+            ret.put("result", false);
+            ret.put("message", "Missing argument sourceName");
+            call.resolve(ret);
+        }
+
+        if(!data.has("dataType")) {
+            ret.put("result", false);
+            ret.put("message", "Missing argument dataType");
+            call.resolve(ret);
+        }
+        String dataType = call.getData().getString("dataType");
+        DataType dt = datatypes.get(dataType);
+        if(dt == null) {
+            ret.put("result", false);
+            ret.put("message", "DataType " + dataType + " not supported");
+            call.resolve(ret);
+        }
+
+        // --- UP TO HERE ---
+        try {
+            final Boolean result = implementation.store(data, dt);
+            ret.put("result", result);
+            ret.put("message", "Not implemented yet.");
+            call.resolve(ret);
+        } catch (Exception exception) {
+
+        }
     }
 }
